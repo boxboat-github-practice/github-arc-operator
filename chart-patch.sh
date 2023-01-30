16d5e5f1a1c9faee10e698623696f99e59873905
mbmcmullen27 <mbmcmullen27@gmail.com>
2023-01-30 14:24:10 -0600

    rename source directory

--- chart-patch.sh
+++ chart-patch.sh
@@ -1,4 +1,4 @@
-filepath="src/helm-charts/actions-runner-controller/templates/manager_role.yaml"
+filepath="simple-arc-operator/helm-charts/actions-runner-controller/templates/manager_role.yaml"
 headStart=$(grep -n "rules:"  $filepath | awk -F':' '{print $1}')
 tailStart=$(($headStart + 1))
 head -n${headStart} $filepath > head
