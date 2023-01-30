7c86a9d3022f67508d64ea03331378531f536520
mbmcmullen27 <mbmcmullen27@gmail.com>
2023-01-30 13:51:12 -0600

    restructure for action

--- chart-patch.sh
+++ chart-patch.sh
@@ -1,5 +1,6 @@
-headStart=$(grep -n "rules:"  helm-charts/actions-runner-controller/templates/manager_role.yaml | awk -F':' '{print $1}')
+filepath="src/helm-charts/actions-runner-controller/templates/manager_role.yaml"
+headStart=$(grep -n "rules:"  $filepath | awk -F':' '{print $1}')
 tailStart=$(($headStart + 1))
-head -n${headStart} helm-charts/actions-runner-controller/templates/manager_role.yaml > head
-tail -n+${tailStart} helm-charts/actions-runner-controller/templates/manager_role.yaml > tail
-cat head overlays/patch.yaml tail 
+head -n${headStart} $filepath > head
+tail -n+${tailStart} $filepath > tail
+cat head overlays/patch.yaml tail > $filepath
